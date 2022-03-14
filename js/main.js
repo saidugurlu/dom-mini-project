65328c6ba6ec0d77534b3d7703a194e57f24941a
saidugurlu <ugurlumsaid@gmx.de>
2022-03-14 13:25:13 +0100

    added inhalt

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-import { capitalizeFirstCharacter, getTodaysDate } from "./tools.js";
+// bu yazim ile tools da bulunan tüm fonksiyonlari otomatik cagirir.
+// Ama kullanirken tools.fonksiyonun adi seklinde kullanmamiz gerekir.
+import * as tools from "./tools.js";
+import { flashcards } from "./data/flashcards.js";
+
 const contentElem = document.querySelector(".content");
+
 contentElem.innerHTML = `
-<p>${capitalizeFirstCharacter("this ist a test")}</p>`;
-<p>${getTodaysDate()}</p>
-`
+ <p>${tools.getTodaysDate()}
+ </p> <div class="flashcards"> 
+ ${flashcards.map((flashcard) => {
+     return ` <div class="flashcard"> <div class="front">
+    <span class="category">${flashcard.category.toUpperCase()}</span>: 
+    ${tools.capitalizeFirstCharacter(flashcard.front)}</div> 
+    <div class="back">${flashcard.back}</div> </div> `;
+   })
+   .join("")} </div> `;
